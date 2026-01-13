dcc69cfa245408222322ce33afec269d5aadba26
Matheus Braschi Haliski <120330198+MatheusHaliski@users.noreply.github.com>
2026-01-13 16:59:41 -0300

    Update route.ts

--- app/api/pin/route.ts
+++ app/api/pin/route.ts
@@ -82,7 +82,7 @@ function buildClearCookie() {
 }
 
 export async function POST(request: Request) {
-  const hash = "$2a$06$8dFxKypJIQHrSE0QAv79beLx4FAy2xvVWWt4KkNnE1IG.wy5Sl4YG";
+  const hash = "$2a$06$eRCKTb96P1Zq1BLyjdz6geRme1OgMnr.JrRsPKA10jnezTfWoGKbW";
   const secret = "GxQ6oQy7h8C2vYz4N4n6a9LZb2P2y9R1e5pVdHcYxM0";
 
   // lê PIN do body
